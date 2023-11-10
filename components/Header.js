55bd5c2f442faea6e2ea0bf30912eb8e9e0cb499
LeMinhMan2809 <lemann.cth@gmail.com>
2023-11-10 19:34:22 +0700

    Config tailwind

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import styled from "styled-components";
+import Center from "./Center";
+
+
+
+export default function Header() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
