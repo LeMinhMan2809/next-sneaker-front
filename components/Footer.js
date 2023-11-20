22d46d478b73bcae73f08305bed3e97128e0431a
LeMinhMan2809 <lemann.cth@gmail.com>
2023-11-20 08:01:30 +0700

    Fix str

--- components/Footer.js
+++ components/Footer.js
@@ -36,7 +36,7 @@ export default function Footer () {
                             </Link>
                            
                             <Link className="flex" href={"/"}>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4 mt-1 mr-1">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4 mt-1 mr-1">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />                               
                                 </svg> 
                                 Khuyến mãi
