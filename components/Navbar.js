29540b343011f5aff18748b7aa63fd7645d7729d
LaThanhTrong <lathanhtrong322002@gmail.com>
2023-12-02 20:09:39 +0700

    category api

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,44 +1,34 @@
-import mongooseConnect from "@/lib/mongoose"
-import { Category } from "@/models/Category"
 import Link from "next/link"
 import { useEffect } from "react"
-export default function Navbar( {rootCategory} ) {
+import axios from "axios"
+export default function Navbar() {
     useEffect(() => {
-        console.log(rootCategory)
-    }, [rootCategory])
+        axios.get('/api/category').then(response => {
+            console.log(response.data)
+        })
+    }, [])
     return (
         <div className="mt-4 bg-black p-3 mb-4">
-                <nav>
-                    <ul className="flex text-white justify-center items-center">
-                        
-                        <li className="">
-                            <Link href="/">Trang chủ</Link>
-                        </li>
-                        <li className="">
-                            <Link href="/nike">
-                                Nike
-                            </Link>
-                        </li>
-                        <li className="">
-                            <a href="">Adidas</a>
-                        </li>
-                        <li className="">
-                            <a href="">Converse</a>
-                        </li>
- 
-                    </ul>
-                </nav>
-            </div>
-    )
-}
+            <nav>
+                <ul className="flex text-white justify-center items-center">
 
-export async function getServerSideProps() {
-  await mongooseConnect()
-  const rootCategory = await Category.find({ parent: null })
-  console.log(rootCategory)
-  return {
-    props: {
-      rootCategory: JSON.parse(JSON.stringify(rootCategory)),
-    },
-  }
+                    <li className="">
+                        <Link href="/">Trang chủ</Link>
+                    </li>
+                    <li className="">
+                        <Link href="/nike">
+                            Nike
+                        </Link>
+                    </li>
+                    <li className="">
+                        <a href="">Adidas</a>
+                    </li>
+                    <li className="">
+                        <a href="">Converse</a>
+                    </li>
+
+                </ul>
+            </nav>
+        </div>
+    )
 }
